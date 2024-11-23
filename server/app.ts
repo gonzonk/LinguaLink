1fb38582f5661209d88699f405120c03ca6942dc
Gurjaap Bal <gurjaapbal1015@gmail.com>
2024-11-23 14:44:01 -0500

    Added Profiling concept

--- server/app.ts
+++ server/app.ts
@@ -1,11 +1,11 @@
-import AuthenticatingConcept from "./concepts/authenticating";
 import FriendingConcept from "./concepts/friending";
 import PostingConcept from "./concepts/posting";
+import ProfilingConcept from "./concepts/profiling";
 import SessioningConcept from "./concepts/sessioning";
 
 // The app is a composition of concepts instantiated here
 // and synchronized together in `routes.ts`.
 export const Sessioning = new SessioningConcept();
-export const Authing = new AuthenticatingConcept("users");
+export const Profiling = new ProfilingConcept("profiles");
 export const Posting = new PostingConcept("posts");
 export const Friending = new FriendingConcept("friends");
