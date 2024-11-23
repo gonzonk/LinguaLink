0e68acd65da2097f7f5fdb59e30714918db2951b
Gonzalo <gonzalojesparzank@gmail.com>
2024-11-23 16:25:36 -0500

    minor hompage formatting (needs routes when pages are avaiable)

--- client/views/HomeView.vue
+++ client/views/HomeView.vue
@@ -12,38 +12,77 @@ const { currentUsername, isLoggedIn } = storeToRefs(useUserStore());
       <h1 v-if="isLoggedIn">Welcome {{ currentUsername }}!</h1>
       <h1 v-else>Please login!</h1>
     </section>
-    <section>
+    <section id="links">
       <RouterLink>
-        <div>
+        <div id="events">
           <h2>Events</h2>
         </div>
       </RouterLink>
       <RouterLink>
-        <div>
+        <div id="dictionary">
           <h2>Dictionary</h2>
         </div>
       </RouterLink>
-      <RouterLink>
-        <div>
-          <h2>Groups</h2>
-        </div>
-      </RouterLink>
-      <RouterLink>
-        <div>
-          <h2>Quizzes</h2>
-        </div>
-      </RouterLink>
-      <RouterLink>
-        <div>
-          <h2>Word of the Day</h2>
-        </div>
-      </RouterLink>
+      <div id="gqw">
+        <RouterLink>
+          <div id="groups">
+            <h2>Groups</h2>
+          </div>
+        </RouterLink>
+        <RouterLink>
+          <div id="quizzes">
+            <h2>Quizzes</h2>
+          </div>
+        </RouterLink>
+        <RouterLink>
+          <div id="wotd">
+            <h2>Word of the Day</h2>
+          </div>
+        </RouterLink>
+      </div>
     </section>
   </main>
 </template>
 
 <style scoped>
-h1 {
-  text-align: center;
+#links {
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+}
+
+#gqw {
+  display: flex;
+  flex-direction: column;
+}
+
+div {
+  background-color: aliceblue;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+#events {
+  width: 100vw;
+}
+
+#dictionary {
+  width: 50vw;
+  height: 50vh;
+}
+
+#groups {
+  width: 50vw;
+  height: 20vh;
+}
+
+#quizzes {
+  width: 50vw;
+  height: 20vh;
+}
+
+#wotd {
+  width: 50vh;
 }
 </style>
