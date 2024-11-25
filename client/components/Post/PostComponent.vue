be84f914ff279b491430c7bbaf6103e1fd94d06b
Matthew Sequeira <msequeir@mit.edu>
2024-11-25 16:50:05 -0500

    Added icons for editing and deleting a post

--- client/components/Post/PostComponent.vue
+++ client/components/Post/PostComponent.vue
@@ -31,8 +31,26 @@ const deletePost = async () => {
 
   <div class="base">
     <menu v-if="props.post.author == currentUsername">
-      <li><button class="btn-small pure-button" @click="emit('editPost', props.post._id)">Edit</button></li>
-      <li><button class="button-error btn-small pure-button" @click="deletePost">Delete</button></li>
+      <li>
+        <!-- Edit Post Button -->
+        <button class="btn-small pure-button" @click="emit('editPost', props.post._id)">
+          <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="20" height="20" viewBox="0 0 16 16">
+            <rect width="1.997" height="5.657" x="10.472" y="1.701" fill="#8852ff" transform="rotate(-45.001 11.47 4.53)"></rect>
+            <polygon fill="#ffa62e" points="12.824,7.176 8.824,3.176 1,11 1,15 5,15"></polygon>
+            <path fill="#e783d4" d="M11.172,0.828l-1.065,1.065l4,4l1.065-1.065c1.105-1.105,1.105-2.895,0-4l0,0 C14.067-0.276,12.276-0.276,11.172,0.828z"></path>
+          </svg>
+        </button>
+      </li>
+      <!-- Delete Post Button -->
+      <li>
+        <button class="btn-small pure-button" @click="deletePost">
+          <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="20" height="20" viewBox="0 0 16 16">
+            <polygon fill="#6a2eff" points="12,4 10,1 6,1 4,4"></polygon>
+            <polygon fill="#9d70ff" points="13,15 3,15 1.5,4 14.5,4"></polygon>
+            <rect width="14" height="2" x="1" y="2" fill="#6a2eff"></rect>
+          </svg>
+        </button>
+      </li>
     </menu>
     <article class="timestamp">
       <p v-if="props.post.dateCreated !== props.post.dateUpdated">Edited on: {{ formatDate(props.post.dateUpdated) }}</p>
@@ -54,7 +72,7 @@ p {
 }
 
 .translation {
-  font-size: 1em; /* Slightly larger font size for titles */
+  font-size: 1em;
   text-align: center;
   color: #333; /* Dark text for contrast */
 }
@@ -76,7 +94,6 @@ p {
   height: 100%;
   object-fit: cover;
   border-radius: 8px;
-  border: 2px solid #6fc5d6; /* Border to match the theme */
 }
 
 .author {
