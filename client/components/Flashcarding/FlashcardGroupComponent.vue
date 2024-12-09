ef39ba89c54f23d8475fa041285d20d0783793db
Matthew Sequeira <msequeir@mit.edu>
2024-12-09 16:32:42 -0500

    Added arrow styling to flashcards

--- client/components/Flashcarding/FlashcardGroupComponent.vue
+++ client/components/Flashcarding/FlashcardGroupComponent.vue
@@ -56,8 +56,31 @@ function leftScroll() {
   </main>
   <main class="flashcarding" v-else>
     <button class="flashcardButton" v-on:click="() => (flashcarding = !flashcarding)">Flashcard Group: {{ props.group.name }}</button>
+    <div>
+      <p>Click the card to flip. Navigate to the next or previous word using the buttons on the sides.</p>
+    </div>
     <div class="cardControls">
-      <button class="changeButton" v-on:click="leftScroll">{{ `<<<` }}</button>
+      <!-- Left scroll button with the new SVG -->
+      <button class="changeButton" v-on:click="leftScroll">
+        <svg
+          fill="#000000"
+          height="50px"
+          width="50px"
+          version="1.1"
+          id="Layer_1"
+          xmlns="http://www.w3.org/2000/svg"
+          xmlns:xlink="http://www.w3.org/1999/xlink"
+          viewBox="0 0 330 330"
+          xml:space="preserve"
+          transform="matrix(-1, 0, 0, 1, 0, 0)"
+        >
+          <path
+            d="M15,180h263.787l-49.394,49.394c-5.858,5.857-5.858,15.355,0,21.213C232.322,253.535,236.161,255,240,255 s7.678-1.465,10.606-4.394l75-75c5.858-5.857,5.858-15.355,0-21.213l-75-75c-5.857-5.857-15.355-5.857-21.213,0 c-5.858,5.857-5.858,15.355,0,21.213L278.787,150H15c-8.284,0-15,6.716-15,15S6.716,180,15,180z"
+          ></path>
+        </svg>
+      </button>
+
+      <!-- Flashcard card -->
       <article class="card" v-on:click="() => (flipped = !flipped)">
         <div class="front" v-if="!flipped">
           <h2>{{ props.group.items[cardIndex].word }}</h2>
@@ -69,10 +92,25 @@ function leftScroll() {
           </div>
         </div>
       </article>
-      <button class="changeButton" v-on:click="rightScroll">{{ `>>>` }}</button>
-    </div>
-    <div>
-      <p>Click the card to flip. {{ "<<<" }} takes you to the previous card. {{ ">>>" }} takes you to the next card.</p>
+
+      <!-- Right scroll button with the original SVG -->
+      <button class="changeButton" v-on:click="rightScroll">
+        <svg
+          fill="#000000"
+          height="50px"
+          width="50px"
+          version="1.1"
+          id="Layer_1"
+          xmlns="http://www.w3.org/2000/svg"
+          xmlns:xlink="http://www.w3.org/1999/xlink"
+          viewBox="0 0 330 330"
+          xml:space="preserve"
+        >
+          <path
+            d="M15,180h263.787l-49.394,49.394c-5.858,5.857-5.858,15.355,0,21.213C232.322,253.535,236.161,255,240,255 s7.678-1.465,10.606-4.394l75-75c5.858-5.857,5.858-15.355,0-21.213l-75-75c-5.857-5.857-15.355-5.857-21.213,0 c-5.858,5.857-5.858,15.355,0,21.213L278.787,150H15c-8.284,0-15,6.716-15,15S6.716,180,15,180z"
+          ></path>
+        </svg>
+      </button>
     </div>
   </main>
 </template>
@@ -87,7 +125,9 @@ main {
 .cardControls {
   display: flex;
   flex-direction: row;
-  align-content: space-around;
+  align-items: center;
+  justify-content: space-between;
+  width: 100%;
 }
 
 main h1 {
@@ -133,7 +173,7 @@ button {
 
 .changeButton {
   display: inline-block;
-  padding: 10px 20px;
+  padding: 10px;
   font-size: 1rem;
   color: #fff;
   background-color: #007bff;
@@ -141,8 +181,6 @@ button {
   border-radius: 4px;
   cursor: pointer;
   margin: 10px 5px;
-  margin-left: 5vw;
-  margin-right: 5vw;
   transition: background-color 0.3s ease;
 }
 
